ce727d1c55c3867ca2ae3827a673a63f8131ee76
Mai The Quyen <mtquyen1002@gmail.com>
2021-11-17 22:01:31 +0700

    list

--- app/src/main/java/com/example/appquanlyhanghoa/ListActivity.java
+++ app/src/main/java/com/example/appquanlyhanghoa/ListActivity.java
@@ -1,54 +1,75 @@
 package com.example.appquanlyhanghoa;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.viewpager2.widget.ViewPager2;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ListActivity extends AppCompatActivity {
-
-
-
+    private EditText edtname, edtID, edttype,edtquantity,edtunit,edtdscribe;
+    private Button btokaddlist;
+    private FloatingActionButton btaddadapter;
+    private RecyclerView rcvObj;
+    private Obj_adapter mObjAdapter;
+    private List<Obj> mlistObj;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
+        Anhxa();
+        dieukhienNut();
+        getdata();
         BottomNavigationView mNavView = findViewById(R.id.botom_nav);
-//        ViewPager2 mViewPager = findViewById(R.id.viewPager2);
-
-
 
         mNavView.setSelectedItemId(R.id.list);
 
         mNavView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
+                switch (item.getItemId()) {
                     case R.id.list:
                         return true;
                     case R.id.scan:
-                        startActivity(new Intent(getApplicationContext(),ScanKit.class));
-                        overridePendingTransition(0,0);
+                        startActivity(new Intent(getApplicationContext(), ScanKit.class));
+                        overridePendingTransition(0, 0);
                         return true;
                     case R.id.info:
-                        startActivity(new Intent(getApplicationContext(),InfoActivity.class));
-                        overridePendingTransition(0,0);
+                        startActivity(new Intent(getApplicationContext(), InfoActivity.class));
+                        overridePendingTransition(0, 0);
                         return true;
                     case R.id.logout:
-                        startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
-                        overridePendingTransition(0,0);
+                        startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+                        overridePendingTransition(0, 0);
                         return true;
 
                 }
@@ -56,8 +77,111 @@ public class ListActivity extends AppCompatActivity {
             }
         });
 
-
+    }
+    private void dieukhienNut() {
+        btaddadapter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clickAdd(Gravity.CENTER);
+            }
+        });
     }
 
+    private void clickAdd(int gravity){
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.activity_addlist);
 
+        Window window = dialog.getWindow();
+        if(window == null)
+            return;
+
+        window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+
+        WindowManager.LayoutParams windowAtributes = window.getAttributes();
+        windowAtributes.gravity = gravity;
+        window.setAttributes(windowAtributes);
+
+        if(Gravity.CENTER == gravity)
+            dialog.setCancelable(true);
+        else
+            dialog.setCancelable(false);
+
+        edtname = dialog.findViewById(R.id.edittextname);
+        edtID = dialog.findViewById(R.id.edittextID);
+        edttype = dialog.findViewById(R.id.edittexttype);
+        edtunit = dialog.findViewById(R.id.edittextunit);
+        edtquantity = dialog.findViewById(R.id.edittextquantity);
+        edtdscribe = dialog.findViewById(R.id.edittextdscribe);
+        btokaddlist = dialog.findViewById(R.id.buttonokaddlist);
+
+        btokaddlist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int id   = Integer.parseInt(edtID.getText().toString().trim());
+                int quantity   = Integer.parseInt(edtquantity.getText().toString().trim());
+                String name = edtname.getText().toString().trim();
+                String type = edttype.getText().toString().trim();
+                String unit = edtunit.getText().toString().trim();
+                String dscribe= edtdscribe.getText().toString().trim();
+                Obj obj = new Obj(id,name,quantity,type,unit,dscribe);
+
+                clickPush(obj);
+                dialog.cancel();
+            }
+        });
+
+        dialog.show();
+    }
+
+    private void clickPush(Obj obj) {
+        FirebaseDatabase database   = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("list_object");
+
+        String pathObject = String.valueOf(obj.getID());
+        myRef.child(pathObject).setValue(obj, new DatabaseReference.CompletionListener() {
+            @Override
+            public void onComplete(@Nullable DatabaseError error, @NonNull DatabaseReference ref) {
+                Toast.makeText(ListActivity.this, "THÊM THÀNH CÔNG", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void getdata(){
+        FirebaseDatabase database   = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("list_object");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                mlistObj.clear();
+                for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    Obj obj = dataSnapshot.getValue(Obj.class);
+                    mlistObj.add(obj);
+                }
+                mObjAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(ListActivity.this, "LẤY DỮ LIỆU THẤT BẠI", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void Anhxa() {
+        btaddadapter = findViewById(R.id.buttonadd);
+
+        rcvObj = findViewById(R.id.rcvObj);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        rcvObj.setLayoutManager(linearLayoutManager);
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        rcvObj.addItemDecoration(dividerItemDecoration);
+
+        mlistObj = new ArrayList<>();
+        mObjAdapter = new Obj_adapter(mlistObj);
+
+        rcvObj.setAdapter(mObjAdapter);
+    }
 }
---
+++ app/src/main/java/com/example/appquanlyhanghoa/Obj_adapter.java
@@ -0,0 +1,62 @@
+package com.example.appquanlyhanghoa;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class Obj_adapter extends RecyclerView.Adapter<Obj_adapter.UserViewHolder>{
+
+    private List<Obj> mListObj;
+    public Obj_adapter(List<Obj> mListObj){
+        this.mListObj = mListObj;
+    }
+    @NonNull
+    @Override
+    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.obj_adapter, parent, false);
+        return new UserViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
+        Obj obj = mListObj.get(position);
+        if(obj == null){
+            return;
+        }
+        holder.tvid.setText("ID: "+ obj.getID());
+        holder.tvname.setText("Name: "+ obj.getName());
+        holder.tvtype.setText("Type: "+ obj.getType());
+        holder.tvquantity.setText("Quantity: "+ obj.getQuantity());
+        holder.tvunit.setText("Unit: "+ obj.getUnit());
+        holder.tvdscribe.setText("Description: "+ obj.getDscribe());
+    }
+
+    @Override
+    public int getItemCount() {
+        if(mListObj != null){
+            return mListObj.size();
+        }
+        return 0;
+    }
+
+    public  class UserViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView tvid, tvname,tvquantity, tvtype, tvunit, tvdscribe;
+        public UserViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvid = itemView.findViewById(R.id.tvid_objadapter);
+            tvquantity = itemView.findViewById(R.id.tvquantity_objadapter);
+            tvname = itemView.findViewById(R.id.tvname_objadapter);
+            tvtype = itemView.findViewById(R.id.tvtype_objadapter);
+            tvunit = itemView.findViewById(R.id.tvunit_objadapter);
+            tvdscribe = itemView.findViewById(R.id.tvdscribe_objadapter);
+
+        }
+    }
+}
